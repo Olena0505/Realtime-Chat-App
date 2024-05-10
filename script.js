273b77d84cdfab0a8335a6d23f8018927cb1496d
Olena Kit <olenakit0505@gmail.com>
2024-05-10 06:33:54 +0300

    Working version finally

--- script.js
+++ script.js
@@ -1,34 +1,159 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const socket = io('http://localhost:4444');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const chatMembersNames = document.getElementById('members-names');
+const roomsList = document.getElementById('chat-rooms-list');
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+const userName = prompt('What is your name?');
+socket.emit('new-user', userName);
+let currentChatRoomId = null;
+let chatRoomList = {};
+let myId = 0;
+
+socket.on('user-connected', data => {
+  const allChatElement = addGroup(0, "All");
+  chatRoomList[0] = [];
+  roomsList.appendChild(allChatElement);
+
+  myId = data.id;
+
+  for (let socketId in data.usersList) {
+    if (socketId === data.id) {
+      continue;
+    }
+    chatRoomList[socketId] = [];
+    const groupElement = addGroup(socketId, data.usersList[socketId]);
+    roomsList.appendChild(groupElement);
+  }
+});
+
+function addGroup(id, name) {
+  const groupElement = document.createElement('div');
+  groupElement.classList.add('room');
+  groupElement.setAttribute('data-id', id);
+  groupElement.setAttribute('data-name', name);
+  groupElement.innerHTML = `
+    <span>${name}</span>
+  `;
+  return groupElement;
+}
 
 socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
+  console.log(data.targetRoomId);
+  console.log(chatRoomList); 
+  chatRoomList[data.targetRoomId].push(data.message);
+  if (data.currentChatRoomId == data.targetRoomId) {
+    appendMessage(data.message);
+  }
+});
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
+socket.on("new-chat", data => {
+  chatRoomList[data.id] = [];
+  let newGroup = addGroup(data.id, data.name);
+  roomsList.append(newGroup);
+
+  if(currentChatRoomId == 0) {
+    appendMember(data.name);
+  }
 })
 
 messageForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const message = messageInput.value
-  appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
-  messageInput.value = ''
+  e.preventDefault();
+  const message = messageInput.value;
+  let messageInfo = {id: myId, senderName: userName, message: message};
+  console.log(chatRoomList);
+  chatRoomList[currentChatRoomId].push(messageInfo);
+  appendSelfMessage(messageInfo);
+
+  console.log("room : " + currentChatRoomId);
+  const dataToSend = {messageInfo: messageInfo, targetRoomId: myId};
+  if(currentChatRoomId == 0){
+    dataToSend.targetRoomId = 0;
+  }
+  socket.emit('send-chat-message', dataToSend);
+
+  messageInput.value = '';
 })
 
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
+function appendMessage(messageData) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = `${messageData.senderName}: ${messageData.message}`;
+  messageContainer.appendChild(messageElement);
 }
+
+function appendSelfMessage(messageData) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = `You: ${messageData.message}`;
+  messageContainer.append(messageElement);
+}
+
+roomsList.addEventListener('click', function(event) {
+  if (event.target.classList.contains('room')) {
+    let id = event.target.getAttribute('data-id');
+    let name = event.target.getAttribute('data-name');
+
+    if (currentChatRoomId == id) {
+      return;
+    }
+
+    currentChatRoomId = id;
+
+    document.querySelector('.chat-name').textContent = `Chat room ${name}`;
+
+    messageContainer.innerHTML = '';
+
+    chatRoomList[id].forEach(messageInfo => {
+      if (messageInfo.id == myId) {
+        appendSelfMessage(messageInfo);
+      } else {
+        appendMessage(messageInfo);
+      }
+    });
+
+    if (id == 0) {
+      socket.emit('get-all-members');
+    } else {
+      let listOfMembers = {};
+      listOfMembers[id] = name;
+      listOfMembers[myId] = userName;
+      setMembers(listOfMembers);
+    }
+  }
+});
+
+
+socket.on('all-members', allUsers => {
+  let listOfMembers = {};
+  listOfMembers = allUsers;
+  setMembers(listOfMembers);
+})
+
+function setMembers(listOfMembers) {
+  while (chatMembersNames.firstChild) {
+    chatMembersNames.removeChild(chatMembersNames.firstChild);
+  }
+  for (let socketId in listOfMembers) {
+    appendMember(listOfMembers[socketId]);
+  }
+}
+
+function appendMember(name) {
+  const memberElement = document.createElement('div');
+  memberElement.classList.add('chat-user');
+  memberElement.innerHTML = `
+    <span>
+      ${name}
+    </span>
+  `;
+  chatMembersNames.appendChild(memberElement);
+}
+
+socket.on('user-disconnected', socketId => {
+  delete chatRoomList[socketId];
+  let chatToDelete = document.querySelector('div[data-id="' + socketId + '"]');
+  if (chatToDelete) {
+    chatToDelete.parentNode.removeChild(chatToDelete);
+  }
+});
--- server.js
+++ server.js
@@ -8,22 +8,34 @@ const io = require('socket.io')(
   }
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = 4444;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 const users = {};
 io.on('connection', socket => {
+
   socket.on('new-user', name => {
     users[socket.id] = name;
-    socket.broadcast.emit('user-connected', name);
+    socket.emit('user-connected', {   
+      id: socket.id,
+      usersList: users
+    });
+    socket.broadcast.emit('new-chat', {id: socket.id, name: users[socket.id]});
   });
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+
+  socket.on('send-chat-message', data => {
+    socket.broadcast.emit('chat-message', { message: data.messageInfo, targetRoomId: data.targetRoomId });
   });
+
+  socket.on('get-all-members', () => {
+    socket.emit('all-members', users);
+  });
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    socket.broadcast.emit('user-disconnected', socket.id);
     delete users[socket.id];
   });
+
 });
